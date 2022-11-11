d00c79b70954c323a473d43a9e8c06adc034950a
ConnorWolfe <Cwolfe25@gcds.net>
2022-11-11 11:53:53 -0500

    Update generic_recursion.py

--- generic_recursion.py
+++ generic_recursion.py
@@ -1,9 +1,9 @@
-def P(a,b):
+def Get_Product_of_2_Whole_Numbers(a,b):
     if b > 0:
-        return a + P(a,b-1)
+        return a + Get_Product_of_2_Whole_Numbers(a,b-1)
     elif b == 0:
         return 0
 
 
 if __name__ == "__main__":
-    print(P(7,4))
+    print(Get_Product_of_2_Whole_Numbers(7,4))
